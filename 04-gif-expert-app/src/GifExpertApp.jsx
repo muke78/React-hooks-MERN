2e776e3176f567e58b63f2b138a07d07dd8dc9d4
Erick Muke <muke7881@gmail.com>
2024-07-09 19:10:52 -0600

    Seccion 6 GiftExpertApp Component
    Se crea el entorno de trabajo para la API de gifts

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* titulo */}
+      <h1>GiftExpertApp</h1>
+
+      {/* input */}
+
+      {/* Listado de Gif  */}
+      {/* Gif Item  */}
+    </>
+  );
+};
