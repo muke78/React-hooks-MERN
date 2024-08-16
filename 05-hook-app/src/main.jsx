e49095a5602bc8c2ca63e71d6193eb4f4e95fea9
Erick Muke <muke7881@gmail.com>
2024-08-16 03:58:52 -0600

    Seccion 9 terminada: Empezamos con el useReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -9,12 +9,14 @@ import { createRoot } from 'react-dom/client';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <MemoHook/>
+  <Padre/>
   // </StrictMode>
 )
