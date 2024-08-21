0062c4169b6a1efdf912bb006dd1519086d2fea6
Erick Muke <muke7881@gmail.com>
2024-08-21 04:58:56 -0600

    Se hara la configuracion de editar en el TODO

--- 05-hook-app/src/08-useReducer/components/TodoItem.jsx
+++ 05-hook-app/src/08-useReducer/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 export const TodoItem = ({ item, onDeleteTodo, onToggleTodo }) => {
   return (
-    <li className="list-group-item d-flex justify-content-between">
+    <li className="list-group-item d-flex justify-content-between align-items-center">
       <span
         className={`align-self-center ${
           item.done ? "text-decoration-line-through" : ""
@@ -10,9 +10,15 @@ export const TodoItem = ({ item, onDeleteTodo, onToggleTodo }) => {
       >
         {item.description}{" "}
       </span>
-      <button className="btn btn-danger" onClick={() => onDeleteTodo(item.id)}>
-        Borrar
-      </button>
+      <div className="ms-auto">
+        <button className="btn btn-primary me-2">Editar</button>
+        <button
+          className="btn btn-danger right"
+          onClick={() => onDeleteTodo(item.id)}
+        >
+          Borrar
+        </button>
+      </div>
     </li>
   );
 };
